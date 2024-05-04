c12bb6ad8f8fdd8a9b6cc46b56c1e87464e2be89
yoskno1 <29268851-yoskno1@users.noreply.replit.com>
2024-05-04 00:26:51 +0000

    My 1 Project

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-  const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f0d4ddf38e49745c2c5cc6cdcd32e8fa&page=1';
-  const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-  const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=f0d4ddf38e49745c2c5cc6cdcd32e8fa&query=";
+// Constants for API endpoints and image paths
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1';
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=f0d4ddf38e49745c2c5cc6cdcd32e8fa&query=";
 
+// DOM elements
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
 
-  const main = document.getElementById("section");
-  const form = document.getElementById("form");
-  const search = document.getElementById("query");
-
-returnMovies(APILINK)
-  function returnMovies(url){
-    fetch(url).then(res => res.json())
-    .then(function(data){
+// Function to fetch and display movies
+returnMovies(APILINK);
+function returnMovies(url){
+  fetch(url).then(res => res.json())
+  .then(function(data){
     console.log(data.results);
     data.results.forEach(element => {
+        // Creating elements for each movie
         const div_card = document.createElement('div');
         div_card.setAttribute('class', 'card');
 
@@ -31,28 +34,31 @@ returnMovies(APILINK)
 
         const center = document.createElement('center');
 
+        // Setting content and attributes
         title.innerHTML = `${element.title}`;
         image.src = IMG_PATH + element.poster_path;
 
+        // Appending elements to create card layout
         center.appendChild(image);
         div_card.appendChild(center);
         div_card.appendChild(title);
         div_column.appendChild(div_card);
         div_row.appendChild(div_column);
 
-        main.appendChild(div_row);
+        main.appendChild(div_row); // Appending the card to main section
     });
   });
-  }
+}
 
+// Event listener for form submission
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  main.innerHTML = '';
+  e.preventDefault(); // Preventing default form submission behavior
+  main.innerHTML = ''; // Clearing previous search results
 
-  const searchItem = search.value;
+  const searchItem = search.value; // Getting the search query
 
   if (searchItem) {
-    returnMovies(SEARCHAPI + searchItem);
-      search.value = "";
+    returnMovies(SEARCHAPI + searchItem); // Fetching movies based on search query
+    search.value = ""; // Clearing the search input field
   }
 });
